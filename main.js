68b512dc9c3c68fa694dbe489a89f5c9a0424199
Rafa Sánchez Lucena <sanchez.lucena@gmail.com>
2022-04-14 17:51:57 +0200

    segundo commit

--- main.js
+++ main.js
@@ -1,14 +1,14 @@
 const div = document.querySelector(".container__reloj");
 
-const mostrarReloj = ()=> {
+const mostrarReloj = () => {
   let fecha = new Date();
   let hora = fecha.getHours();
   let minutos = fecha.getMinutes();
   let segundos = fecha.getSeconds();
-  if (hora < 10) hora = "0" + hora;
-  if (minutos < 10) minutos = "0" + minutos;
-  if (segundos < 10) segundos = "0" + segundos;
-  div.innerHTML = `${hora} : ${minutos} : ${segundos}`
+  hora < 10 ? hora = "0" + hora : hora;
+  minutos < 10 ? minutos = "0" + minutos : minutos;
+  segundos < 10 ? segundos = "0" + segundos : segundos;
+  div.innerHTML = `${hora} : ${minutos} : ${segundos}`;
 };
 
 setInterval(mostrarReloj, 1000);
